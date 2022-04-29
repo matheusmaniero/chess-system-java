65a02676ab6d48b57e5c67e18487f3c32b929431
Matheus Maniero <matheusmaniero@gmail.com>
2022-04-29 07:45:40 -0300

    First Class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,20 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		
 
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		Position p1 = new Position(2, 5);
+		System.out.println(p1);
+
+		sc.close();
 	}
 
 }
